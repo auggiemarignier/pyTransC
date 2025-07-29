ef541ba5219560a57838962cd45a269b5ac78406
auggiemarignier <auggie.marignier@earth.ox.ac.uk>
2025-07-29 10:10:29 +0100

    refactor: Break up Ensemble Resampler
    Squashed commit of the following:
    
    commit 906f3793f1ff393cbf921d467da8662b51c00697
    Author: auggiemarignier <auggie.marignier@earth.ox.ac.uk>
    Date:   Tue Jul 29 10:00:06 2025 +0100
    
        refactor: separate parallel and serial
    
    commit fd4012f802060fcbcab1d616581c4b14d430ee5d
    Author: auggiemarignier <auggie.marignier@earth.ox.ac.uk>
    Date:   Tue Jul 29 09:48:42 2025 +0100
    
        style: type hints
    
    commit 7cdc4dbc151f128e734ffed156eb454fa4ed9045
    Author: auggiemarignier <auggie.marignier@earth.ox.ac.uk>
    Date:   Fri Jul 25 16:20:04 2025 +0100
    
        refactor: break up chain_step
    
    commit 4e5e6b827c833f010748200247553645731f0c4f
    Author: auggiemarignier <auggie.marignier@earth.ox.ac.uk>
    Date:   Fri Jul 25 15:02:08 2025 +0100
    
        feat: simliar dataclasses as for state_jump
    
    commit 8305091c366b95142e60e06c39a24f03d5bee75b
    Author: auggiemarignier <auggie.marignier@earth.ox.ac.uk>
    Date:   Fri Jul 25 13:57:24 2025 +0100
    
        fix: use logger
    
    commit de57dae91811e011862e64bdd3b33d264abdd069
    Author: auggiemarignier <auggie.marignier@earth.ox.ac.uk>
    Date:   Fri Jul 25 13:57:24 2025 +0100
    
        fix: remove _my_range

--- src/pyTransC/samplers/ensemble_resampler.py
+++ src/pyTransC/samplers/ensemble_resampler.py
@@ -1,25 +1,163 @@
 """Ensemble resampler for TransC."""
 
+import logging
 import multiprocessing
 import random
+from dataclasses import dataclass, field
 from functools import partial
 
 import numpy as np
 from tqdm import tqdm
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class Sample:
+    """Convenience dataclass to hold a single sample from the ensemble resampler.
+
+    This only deals with indices of the member in the ensemble and its state index.
+    """
+
+    member: int  # index of the model in the ensemble
+    state: int
+
+
+@dataclass
+class EnsembleResamplerChain:
+    """Data class to hold the results of the ensemble resampler."""
+
+    n_states: int  # This could be inferred from state_chain assuming every state is visited at least once.  Requiring it at initialisation makes it more robust for downstream tasks.
+    member_chain: list[int] = field(default_factory=list)
+    state_chain: list[int] = field(default_factory=list)
+    n_proposed: int = 0
+    n_accepted: int = 0
+
+    def __post_init__(self):
+        """Post-initialization checks."""
+        if not isinstance(self.n_states, int) or self.n_states <= 0:
+            raise ValueError("n_states must be a positive integer.")
+        if len(self.member_chain) != len(self.state_chain):
+            raise ValueError("Member chain and state chain must have the same length.")
+        if len(self.state_chain) != self.n_steps:
+            raise ValueError(
+                "Total proposals must be equal to the length of the state and member chains."
+            )
+        if self.n_accepted > self.n_proposed:
+            raise ValueError(
+                "You somehow accepted more between-state proposals than you made."
+            )
+
+    @property
+    def state_chain_tot(self) -> list[list[int]]:
+        """Running cumulative tally of states visited."""
+
+        state_chain_tot: list[list[int]] = []
+        visits: list[int] = [0] * self.n_states
+        for state in self.state_chain:
+            visits[state] += 1
+            state_chain_tot.append(visits.copy())
+        return state_chain_tot
+
+    @property
+    def n_steps(self) -> int:
+        """Number of steps in the chain, calculated as the total number of proposals."""
+        return self.n_proposed
+
+
+def update_chain(
+    chain: EnsembleResamplerChain,
+    sample: Sample,
+    proposal_accepted: bool,
+) -> None:
+    """Update the chain with a new sample.
+
+    Args:
+        chain (EnsembleResamplerChain): The chain to update.
+        sample (Sample): The new sample to add.  Note that this sample is the outcome of the acceptance/rejection step i.e. it will be the previous sample if the proposal was rejected.
+        proposal_accepted (bool): Whether the proposal was accepted or not.
+    """
+    chain.member_chain.append(sample.member)
+    chain.state_chain.append(sample.state)
+    chain.n_accepted += int(proposal_accepted)
+    chain.n_proposed += 1
+
+
+@dataclass
+class MultiWalkerEnsembleResamplerChain:
+    """Class to hold and manage multiple ensemble resampler chains from different walkers."""
+
+    chains: list[EnsembleResamplerChain] = field(default_factory=list)
+
+    def __post_init__(self):
+        """Post-initialization checks."""
+        if not self.chains:
+            # no chains, not a problem
+            return
+
+        if any(not isinstance(chain, EnsembleResamplerChain) for chain in self.chains):
+            raise TypeError("All chains must be instances of EnsembleResamplerChain.")
+
+        n_states = self.chains[0].n_states
+        if any(chain.n_states != n_states for chain in self.chains[1:]):
+            raise ValueError("All chains must have the same number of states.")
+
+    @property
+    def n_walkers(self) -> int:
+        """Number of walkers in the multi-walker chain."""
+        return len(self.chains)
+
+    @property
+    def n_states(self) -> int:
+        """Number of states in the multi-walker chain."""
+        if self.chains:
+            return self.chains[0].n_states
+        return 0
+
+    @property
+    def member_chain(self) -> list[list[int]]:
+        """Concatenated member index chain from all walkers."""
+        return [chain.member_chain for chain in self.chains]
+
+    @property
+    def state_chain(self) -> np.ndarray:
+        """Concatenated state chain from all walkers."""
+        return np.array([chain.state_chain for chain in self.chains])
+
+    @property
+    def state_chain_tot(self) -> np.ndarray:
+        """Concatenated total state chain from all walkers."""
+        return np.array([chain.state_chain_tot for chain in self.chains])
+
+    @property
+    def n_accepted(self) -> np.ndarray:
+        """Number of accepted proposals for each walker."""
+        return np.array([chain.n_accepted for chain in self.chains])
+
+    @property
+    def n_proposed(self) -> np.ndarray:
+        """Number of proposals for each walker."""
+        return np.array([chain.n_proposed for chain in self.chains])
+
 
 def run_ensemble_resampler(  # Independent state Marginal Likelihoods from pre-computed posterior and pseudo prior ensembles
     n_walkers,
     n_steps,
+    n_states: int,
     n_dims: list[int],
     log_posterior_ens,
     log_pseudo_prior_ens,
     seed=61254557,
     parallel=False,
     n_processors=1,
-    state_proposal_weights=None,
+    state_proposal_weights: list[list[float]] | None = None,
     progress=False,
-):
+) -> MultiWalkerEnsembleResamplerChain:
     """
     MCMC sampler over independent states using a Markov Chain.
 
@@ -51,156 +189,235 @@ def run_ensemble_resampler(  # Independent state Marginal Likelihoods from pre-c
 
     """
 
-    n_states = len(log_posterior_ens)
     n_samples = [len(log_post_ens) for log_post_ens in log_posterior_ens]
 
-    print("\nRunning ensemble resampler")
-    print("\nNumber of walkers               : ", n_walkers)
-    print("Number of states being sampled  : ", n_states)
-    print("Dimensions of each state        : ", n_dims)
+    if state_proposal_weights is None:
+        # uniform proposal weights
+        _state_proposal_weights = [[1.0] * n_states] * n_states
+    else:
+        _state_proposal_weights = np.array(state_proposal_weights)
+        np.fill_diagonal(_state_proposal_weights, 0.0)  # ensure diagonal is zero
+        _state_proposal_weights = _state_proposal_weights / _state_proposal_weights.sum(
+            axis=1, keepdims=True
+        )  # set row sums to unity
+        _state_proposal_weights = _state_proposal_weights.tolist()
+
+    logger.info("\nRunning ensemble resampler")
+    logger.info("\nNumber of walkers               : %d", n_walkers)
+    logger.info("Number of states being sampled  : %d", n_states)
+    logger.info("Dimensions of each state        : %s", n_dims)
 
     random.seed(seed)
-    state_chain_tot = np.zeros((n_walkers, n_steps, n_states), dtype=int)
-    state_chain = np.zeros((n_walkers, n_steps), dtype=int)
-    accept_between = np.zeros(n_walkers, dtype=int)
     if parallel:
-        if n_processors == 1:
-            n_processors = (
-                multiprocessing.cpu_count()
-            )  # set number of processors equal to those available
-        chunk_size = int(np.ceil(n_walkers / n_processors))  # set work per processor
-        jobs = random.choices(
-            range(n_states), k=n_walkers
-        )  # input data for parallel jobs
-        func = partial(
-            _mcmc_walker_ens,  # create reduced one argument function for passing to pool.map())
-            n_states=n_states,
-            n_samples=n_samples,
-            n_steps=n_steps,
-            log_posterior_ens=log_posterior_ens,
-            log_pseudo_prior_ens=log_pseudo_prior_ens,
-            state_proposal_weights=state_proposal_weights,
+        chains = _run_mcmc_walker_parallel(
+            n_walkers,
+            n_states,
+            n_samples,
+            n_steps,
+            log_posterior_ens,
+            log_pseudo_prior_ens,
+            state_proposal_weights=_state_proposal_weights,
+            n_processors=n_processors,
+            progress=progress,
         )
-        result = []
-        if progress:
-            with multiprocessing.Pool(processes=n_processors) as pool:
-                res = list(
-                    tqdm(pool.imap(func, jobs, chunksize=chunk_size), total=len(jobs))
-                )
-        else:
-            pool = multiprocessing.Pool(processes=n_processors)
-            res = pool.map(func, jobs, chunksize=chunk_size)
-        result.append(res)
+
+    else:
+        chains = _run_mcmc_walker_serial(
+            n_walkers,
+            n_states,
+            n_samples,
+            n_steps,
+            log_posterior_ens,
+            log_pseudo_prior_ens,
+            state_proposal_weights=_state_proposal_weights,
+        )
+
+    return MultiWalkerEnsembleResamplerChain(chains)
+
+
+def _run_mcmc_walker_parallel(
+    n_walkers: int,
+    n_states: int,
+    n_samples: list[int],
+    n_steps: int,
+    log_posterior_ens: list[list[float]],
+    log_pseudo_prior_ens: list[list[float]],
+    state_proposal_weights: list[list[float]],
+    n_processors: int = 1,
+    progress: bool = False,
+) -> list[EnsembleResamplerChain]:
+    """Run the ensemble resampler in parallel using multiprocessing.
+
+    THIS HAS NOT BEEN TESTED YET.
+    """
+    if n_processors == 1:
+        # set number of processors equal to those available
+        n_processors = multiprocessing.cpu_count()
+
+    # set work per processor
+    chunk_size = int(np.ceil(n_walkers / n_processors))
+
+    # input data for parallel jobs
+    jobs = random.choices(range(n_states), k=n_walkers)
+
+    # create reduced one argument function for passing to pool.map())
+    func = partial(
+        _mcmc_walker,
+        n_states=n_states,
+        n_samples=n_samples,
+        n_steps=n_steps,
+        log_posterior_ens=log_posterior_ens,
+        log_pseudo_prior_ens=log_pseudo_prior_ens,
+        state_proposal_weights=state_proposal_weights,
+    )
+
+    # run the parallel jobs
+    if progress:
+        with multiprocessing.Pool(processes=n_processors) as pool:
+            chains: list[EnsembleResamplerChain] = list(
+                tqdm(pool.imap(func, jobs, chunksize=chunk_size), total=len(jobs))
+            )
+    else:
+        pool = multiprocessing.Pool(processes=n_processors)
+        chains: list[EnsembleResamplerChain] = pool.map(
+            func, jobs, chunksize=chunk_size
+        )
         pool.close()
         pool.join()
-        for i in range(n_walkers):  # decode the output
-            state_chain_tot[i] = result[0][i][1]
-            state_chain[i] = result[0][i][0]
-            accept_between[i] = result[0][i][2]
 
-        pass
-    else:
-        for walker in _my_range(progress, n_walkers):
-            cstate = random.choice(
-                range(n_states)
-            )  # choose initial current state randomly
-            state_chain[walker], state_chain_tot[walker], accept_between[walker] = (
-                _mcmc_walker_ens(
-                    n_states,
-                    n_samples,
-                    cstate,
-                    n_steps,
-                    log_posterior_ens,
-                    log_pseudo_prior_ens,
-                    state_proposal_weights=state_proposal_weights,
-                )
-            )  # carry out an mcmc walk between ensembles
-
-    return state_chain, state_chain_tot, accept_between
+    return chains
 
 
-def _mcmc_walker_ens(
+def _run_mcmc_walker_serial(
+    n_walkers: int,
+    n_states: int,
+    n_samples: list[int],
+    n_steps: int,
+    log_posterior_ens: list[list[float]],
+    log_pseudo_prior_ens: list[list[float]],
+    state_proposal_weights: list[list[float]],
+    progress: bool = False,
+) -> list[EnsembleResamplerChain]:
+    chains: list[EnsembleResamplerChain] = []
+    for _ in tqdm(range(n_walkers), disable=not progress):
+        # choose initial current state randomly
+        current_state = random.choice(range(n_states))
+
+        # carry out an mcmc walk between ensembles
+        chains.append(
+            _mcmc_walker(
+                n_states,
+                n_samples,
+                current_state,
+                n_steps,
+                log_posterior_ens,
+                log_pseudo_prior_ens,
+                state_proposal_weights=state_proposal_weights,
+            )
+        )
+
+    return chains
+
+
+def _mcmc_walker(
     n_states: int,
     n_samples: list[int],
     current_state,
     n_steps,
     log_posterior_ens,
     log_pseudo_prior_ens,
-    state_proposal_weights=None,
-    verbose=False,
+    state_proposal_weights: list[list[float]],
 ):
     """Internal one chain MCMC sampler used by run_ensemble_resampler()."""
 
-    visits = np.zeros(n_states)
-    state_chain_tot = np.zeros((n_steps, n_states), dtype=int)
-    state_chain = np.zeros((n_steps), dtype=int)
     current_member = random.choice(
         range(n_samples[current_state])
     )  # randomly choose ensemble member from current state
-    visits[current_state] += 1
-    state_chain[0] = current_state  # record initial state for this step and walker
-    state_chain_tot[0] = visits  # record initial current state visited by chain
-    accept = 0
-    if state_proposal_weights is None:
-        state_proposal_weights = np.ones((n_states, n_states))
-    else:
-        np.fill_diagonal(state_proposal_weights, 0.0)  # ensure diagonal is zero
-        state_proposal_weights = state_proposal_weights / state_proposal_weights.sum(
-            axis=1, keepdims=1
-        )  # set row sums to unity
-
-    for chain_step in range(n_steps - 1):  # loop over markov chain steps
-        states = list(range(n_states))  # list of all states
-        states.remove(current_state)  # list of available states
-        weights = state_proposal_weights[
-            current_state, np.delete(np.arange(n_states), current_state)
-        ]
-        proposed_state = random.choices(states, weights=weights)[
-            0
-        ]  # choose proposed state
-        proposed_member = random.choice(
-            range(n_samples[proposed_state])
-        )  # randomly select ensemble member from proposed state
-
-        log_pseudo_prior_current = log_pseudo_prior_ens[current_state][current_member]
-        log_pseudo_prior_proposed = log_pseudo_prior_ens[proposed_state][
-            proposed_member
-        ]
-        log_posterior_current = log_posterior_ens[current_state][current_member]
-        log_posterior_proposed = log_posterior_ens[proposed_state][proposed_member]
-
-        log_proposal_prob = np.log(
-            state_proposal_weights[proposed_state, current_state]
-        ) - np.log(state_proposal_weights[current_state, proposed_state])
-
-        log_proposal_ratio = (
-            log_posterior_proposed
-            + log_pseudo_prior_current
-            - log_posterior_current
-            - log_pseudo_prior_proposed
-            + log_proposal_prob
-        )  # Metropolis-Hastings acceptance criteria
-
-        if log_proposal_ratio >= np.log(random.random()):  # Accept move between states
-            visits[proposed_state] += 1
-            current_state = np.copy(proposed_state)
-            current_member = np.copy(proposed_member)
-            accept += 1
-        else:
-            # Reject move between states
-            visits[current_state] += 1
-
-        state_chain[chain_step + 1] = (
-            current_state  # record state for this step and walker
+    sample = Sample(current_member, current_state)
+    chain = EnsembleResamplerChain(n_states)
+    for _ in range(n_steps - 1):  # loop over markov chain steps
+        sample, accepted = _chain_step(
+            n_states,
+            n_samples,
+            sample,
+            log_posterior_ens,
+            log_pseudo_prior_ens,
+            state_proposal_weights,
         )
-        state_chain_tot[chain_step + 1] = (
-            visits  # record current state visited by chain
-        )
+        update_chain(chain, sample, accepted)
 
-    return state_chain, state_chain_tot, accept
+    return chain
 
 
-def _my_range(progress: bool, length: int):
-    if progress:
-        return tqdm(range(length))
-    return range(length)
+def _chain_step(
+    n_states: int,
+    n_samples: list[int],
+    current: Sample,
+    log_posterior_ens: list[list[float]],
+    log_pseudo_prior_ens: list[list[float]],
+    state_proposal_weights: list[list[float]],  # 2D q(k|k') matrix
+) -> tuple[Sample, bool]:
+    proposed = _propose_sample(
+        current.state, n_states, n_samples, state_proposal_weights
+    )
+
+    log_prob_proposed = _log_prob_sample(
+        proposed, log_posterior_ens, log_pseudo_prior_ens
+    )
+    log_prob_current = _log_prob_sample(
+        current, log_posterior_ens, log_pseudo_prior_ens
+    )
+
+    log_proposal_prob = np.log(
+        state_proposal_weights[proposed.state][current.state]
+    ) - np.log(state_proposal_weights[current.state][proposed.state])
+
+    # Metropolis-Hastings acceptance criteria
+    log_proposal_ratio = log_prob_proposed - log_prob_current + log_proposal_prob
+    accept = log_proposal_ratio >= np.log(random.random())
+
+    next_ = proposed if accept else current
+
+    return next_, accept
+
+
+def _propose_sample(
+    current_state: int,
+    n_states: int,
+    n_samples: list[int],
+    weights: list[list[float]],
+) -> Sample:
+    """Propose a new sample based on the current state and weights."""
+    proposed_state = _propose_state(current_state, n_states, weights)
+    proposed_member = _propose_member_in_state(n_samples[proposed_state])
+    return Sample(proposed_member, proposed_state)
+
+
+def _propose_state(
+    current_state: int, n_states: int, weights: list[list[float]]
+) -> int:
+    """Propose a new state based on the current state."""
+    inactive_states = [i for i in range(n_states) if i != current_state]
+    _weights = [weights[current_state][i] for i in inactive_states]
+    return random.choices(inactive_states, weights=_weights)[0]
+
+
+def _propose_member_in_state(n_samples: int) -> int:
+    """Propose a new member within the current state.
+
+    Args:
+        n_samples (int): Number of samples in the current state.
+    """
+    return random.choice(range(n_samples))
+
+
+def _log_prob_sample(
+    sample: Sample,
+    log_posterior_ens: list[list[float]],
+    log_pseudo_prior_ens: list[list[float]],
+) -> float:
+    """Calculate the log probability of a sample."""
+    return (
+        log_posterior_ens[sample.state][sample.member]
+        - log_pseudo_prior_ens[sample.state][sample.member]
+    )
